8a1966a1b3fa379e3ba6b4785f3035f408435f57
imsecai <wei695153881@126.com>
2018-02-02 21:07:44 +0800

    注册登录样式初步完成

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('compileSass',function(){
+    gulp.src('./src/sass/*.scss')
+
+    .pipe(sass())
+
+    .pipe(gulp.dest('./src/css/'))
+});
+
+gulp.task('jtSass',function(){
+    gulp.watch('./src/sass/*.scss',['compileSass'])
+})
+
+// var browserSync = require('browser-sync');
+
+// gulp.task('server',function(){
+//     browserSync({
+
+//         server:'./src/',
+//         port:1008,
+
+//         files:['./src/**/*.html','./src/css/*.css']
+//     });
+
+//     gulp.watch('./src/scss/*.scss',['compileSass']);
+// })

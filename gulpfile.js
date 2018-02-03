b838e76ab61a90f92eae962e1bca49fece97613a
imsecai <wei695153881@126.com>
2018-02-03 16:58:53 +0800

    登录注册样式完善

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 gulp.task('compileSass',function(){
     gulp.src('./src/sass/*.scss')
 
-    .pipe(sass())
+    .pipe(sass({outputStyle:'expanded'}))
 
     .pipe(gulp.dest('./src/css/'))
 });
